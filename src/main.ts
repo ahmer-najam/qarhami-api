cf22dff739bbc1b34d1775ea383ee586c524fef1
ahmer-najam <53119340+ahmer-najam@users.noreply.github.com>
2024-05-27 12:40:12 +0500

    VercelCors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const myLoggerService = app.get(ExceptionLoggerService);
   app.useGlobalFilters(new AppExceptionFilter(myLoggerService));
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://ahami-app.vercel.app/',
+      'http://localhost:5173',
+      'http://localhost:4173',
+    ],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+  });
 
   await app.listen(3200);
 }
