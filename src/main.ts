8428beda668ab5b2f5b871b8425c33ddf9dea31a
ahmer-najam <53119340+ahmer-najam@users.noreply.github.com>
2024-05-27 12:50:48 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const myLoggerService = app.get(ExceptionLoggerService);
   app.useGlobalFilters(new AppExceptionFilter(myLoggerService));
-  app.enableCors({
-    origin: [
-      'https://ahami-app.vercel.app/',
-      'https://qarhami-pwa-app.vercel.app',
-      'http://localhost:5173',
-      'http://localhost:4173',
-    ],
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders: '*',
-  });
+  app.enableCors();
+  // app.enableCors({
+  //   origin: [
+  //     'https://ahami-app.vercel.app/',
+  //     'https://qarhami-pwa-app.vercel.app',
+  //     'http://localhost:5173',
+  //     'http://localhost:4173',
+  //   ],
+  //   methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+  //   credentials: true,
+  //   allowedHeaders: '*',
+  // });
 
   await app.listen(3200);
 }
